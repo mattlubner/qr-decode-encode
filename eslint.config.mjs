cf01853cec9db5532b4ce1cae3a1a8fd57ea94d9
Matt Lubner <matt@mattlubner.com>
2026-09-25 19:21:32 -0400

    fix(deps): modernize eslint config and migrate off eslint-config-typescript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,11 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default defineConfig([globalIgnores(["**/lib", "**/node_modules"]), {
-    extends: compat.extends("typescript", "plugin:prettier/recommended", "prettier"),
-}]);
+export default defineConfig([
+    globalIgnores(["**/lib"]),
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    eslintPluginPrettierRecommended
+]);

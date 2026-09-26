da44e7ba1682ea7b46ff96b9ceef69010b3237d2
Matt Lubner <matt@mattlubner.com>
2026-09-25 21:44:39 -0400

    fix(deps): remove adhoc type defs fro javascript-barcode-reader

--- src/javascript-barcode-reader.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'javascript-barcode-reader' {
-  /// <reference path="../node_modules/javascript-barcode-reader/dist/types/src/index.d.ts" />
-  export default function javascriptBarcodeReader({
-    image,
-    barcode,
-    barcodeType,
-    options,
-  }: JavascriptBarcodeReader): Promise<string>;
-}
